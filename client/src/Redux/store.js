baa8a81739a7cffd40cbbee3d1f6d659b737ecf0
Naveenvk29 <naveenvinodkumar29@gmail.com>
2024-07-09 11:53:40 +0530

    added store and User,genre api-Query

---
+++ client/src/Redux/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query/react";
+import { apiSlice } from "./Api/ApiSlice";
+import authReducer from "./features/authSlice";
+
+const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+});
+setupListeners(store.dispatch);
+
+export default store;
