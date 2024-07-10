a0864b8bf26dfa2eb7fd7d41344eb1651879c5eb
Naveenvk29 <naveenvinodkumar29@gmail.com>
2024-07-11 00:07:12 +0530

    small change

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api/": "https://movie-review-api-2x8z.onrender.com",
-      "/uploads/": "https://movie-review-api-2x8z.onrender.com",
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api/": "https://movie-review-api-2x8z.onrender.com",
+  //     "/uploads/": "https://movie-review-api-2x8z.onrender.com",
+  //   },
+  // },
 });
